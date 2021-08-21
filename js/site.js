4376712760313feefd5dd5af6a058b7b3402c875
RicardoCruz21 <abstergo21@gmail.com>
2021-08-21 00:47:40 -0500

    rev1 js coded and customized to palindrome project

--- js/site.js
+++ js/site.js
@@ -1,4 +1,58 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hellow World");
+//Get the string from the page
+function getString(){
+    
+    //hide the alert box
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+
+    //Call reverseString function
+    let revString = reverseString(userString);
+
+    let palindrome = isPalindrome(userString, revString);
+
+    if (palindrome) {
+        
+        //Call displayString function
+        displayPalindrome(revString);
+
+    } else {
+        alert("Sorry! Your phrase is not a palidrome.")
+    }
+}
+
+//Reverse the string
+function reverseString(userString){
+
+    let revString = [];
+
+    //Reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];        
+    }
+
+    return revString;
+}
+
+//Check to see if string is a palindrome
+function isPalindrome(string1, string2){
+    
+    for (let index = 0; index < string1.length; index++) {
+                
+        if (string1[index] != string2[index]) {
+                
+            return false;
+        }        
+    }
+    return true;
+}
+
+//Display the reversed string to the page
+function displayPalindrome(revString){
+
+    //write the string to the page
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${revString}`;
+
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
